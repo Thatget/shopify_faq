b1d96ca415ff950e10e5fd81dde3f875364b3a08
Your Name <quyet.nguyen.7712826@gmail.com>
2022-05-18 01:07:49 +0700

    get product list

--- app/helpers/shopifyApi.helper.js
+++ app/helpers/shopifyApi.helper.js
@@ -1,37 +1,23 @@
-const apiKey = process.env.SHOPIFY_API_KEY;
-const apiSecret = process.env.SHOPIFY_API_SECRET;
+const request = require('request-promise');
+const db = require("../models/index");
+const User = db.user;
+
 const errorLog = require('../helpers/log.helper');
 
-const accessTokenPayload = {
-    client_id: apiKey,
-    client_secret: apiSecret,
-    code
-};
-
 const productList = async (req, res) => {
-    const shop = req.params.shop;
-    const accessTokendRequestUrl = 'https://' + shop + '/admin/oauth/access_token';
-    await request.post(accessTokendRequestUrl, { json: accessTokenPayload })
-        .then( async (accessTokenResponse) => {
-            const accessToken = accessTokenResponse.access_token;
-            const shopRequestUrl = 'https://' + shop + '/admin/api/2022-01/products.json';
-            const shopRequestHeaders = {
-                'X-Shopify-Access-Token': accessToken
-            };
-            await request.get(shopRequestUrl, {headers: shopRequestHeaders})
-                .then( async (shopResonse) => {
-                    console.log(shopResonse)
-                    console.log('end line')
-                    console.log(JSON.parse(shopResonse))
+    const id = req.jwtDecoded.data.user_id;
+    try {
+        const userInfo = await User.findByPk(id, {attributes: ['shopify_domain', 'shopify_access_token']});
 
-                })
-                .catch((error) => {
-                    errorLog.error(`get product list: ${error.message}`)});
-        }).catch((e) => {
-            errorLog.error(`get access token in product list: ${e.message}`)
-        });
-    return res.status(200).json({accessToken, refreshToken});
-    return res.status(500).json(error);
+        const shopRequestUrl = 'https://' + userInfo.dataValues.shopify_domain + '/admin/api/2022-01/products.json';
+        const shopRequestHeaders = {
+            'X-Shopify-Access-Token': userInfo.dataValues.shopify_access_token
+        };
+        products= await request.get(shopRequestUrl, {headers: shopRequestHeaders});
+    } catch (e) {
+        return res.status(500).json(e);
+    }
+    return res.status(200).json({products});
 }
 
 module.exports = {
