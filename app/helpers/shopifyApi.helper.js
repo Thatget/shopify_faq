128b02bd544aaadbae1a3386ddd9e478a1c7e740
Your Name <quyet.nguyen.7712826@gmail.com>
2022-05-20 01:46:44 +0700

    get product with limt

--- app/helpers/shopifyApi.helper.js
+++ app/helpers/shopifyApi.helper.js
@@ -2,18 +2,33 @@ const request = require('request-promise');
 const db = require("../models/index");
 const User = db.user;
 
-const errorLog = require('../helpers/log.helper');
-
 const productList = async (req, res) => {
     const id = req.jwtDecoded.data.user_id;
+    let limit = Number(req.query.limit);
+    if (!Number.isInteger(limit)) {
+        res.status(500).send({
+            message: "Limit need interger type"
+        });
+        return;
+    }
+    limit = Math.abs(limit)
     try {
         const userInfo = await User.findByPk(id, {attributes: ['shopify_domain', 'shopify_access_token']});
 
         const shopRequestUrl = 'https://' + userInfo.dataValues.shopify_domain + '/admin/api/2022-01/products.json';
-        const shopRequestHeaders = {
-            'X-Shopify-Access-Token': userInfo.dataValues.shopify_access_token
+
+        var options = {
+            uri: shopRequestUrl,
+            qs: {
+                limit: limit // -> uri + '?access_token=xxxxx%20xxxxx'
+            },
+            headers: {
+                'X-Shopify-Access-Token': userInfo.dataValues.shopify_access_token
+            },
+            json: true // Automatically parses the JSON string in the response
         };
-        products= await request.get(shopRequestUrl, {headers: shopRequestHeaders});
+
+        products = await request(options)
     } catch (e) {
         return res.status(500).json(e);
     }
