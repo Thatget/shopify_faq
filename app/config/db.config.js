4d0cfca6b545aaea2169a3a9d3f11fe8f7e13376
DungPham <phamanhdung593@gmail.com>
2022-06-03 15:15:55 +0700

    refs

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,8 +1,8 @@
 module.exports = {
     HOST: "localhost",
-    USER: "root",
-    PASSWORD: "Matkhau1",
-    DB: "mydb",
+    USER: "dev_newuser",
+    PASSWORD: "dev_password",
+    DB: "dev_faq_app",
     dialect: "mysql",
     pool: {
         max: 5,
