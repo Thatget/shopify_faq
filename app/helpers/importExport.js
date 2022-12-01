ff3c1bf7db2d84eefd39d9f43d89e8334628f6ce
DungPham <phamanhdung593@gmail.com>
2022-12-01 09:11:40 +0700

    refs

--- app/helpers/importExport.js
+++ app/helpers/importExport.js
@@ -3,7 +3,7 @@ const Faq = db.faq;
 const Category = db.faq_category;
 const { QueryTypes } = require('sequelize');
 const errorLog = require('../helpers/log.helper');
-
+let localeDefault = 'default'
 const XLSX = require("xlsx");
 
 
@@ -11,47 +11,128 @@ exports.import = async (req, res) => {
     const wb = XLSX.read(req.file.buffer);
     const sheets = wb.SheetNames;
     const user_id = req.jwtDecoded.data.user_id;
-    var responseResult = {}
-
+    let allCategory = []
+    let allFaq = []
+    // var responseResult = {}
     if(sheets.length > 0) {
+        let categoryCreate = []
+        let categoryOrigin = []
+        let faqCreate = []
         const data = XLSX.utils.sheet_to_json(wb.Sheets[sheets[0]]);
-        console.log(data)
+        // console.log(data)
+            // Prepare for category data
+        allCategory = await getAllCategory(user_id)
+        allFaq = await getAllFaq(user_id)
+        // console.log(allFaq)
+        let category = []
+        let faqs = []
+        var checkExitsCategory
+        data.forEach(item => {
+            item.user_id = user_id
+            item.category_identify = localeDefault + user_id
+        })
+        data.forEach(item => {
+            checkExitsCategory = true
+            let dataFaqPush = {
+                user_id : item.user_id,
+                title: item.Question,
+                content: item.Answer,
+                locale: localeDefault,
+                category_name: item.Category
+            }
+            faqs.push(dataFaqPush)
+            if(category.length === 0) {
+                checkExitsCategory = false
+                let dataPush = {
+                    user_id : item.user_id,
+                    title: item.Category,
+                    is_visible: item.Category_visible,
+                    locale: localeDefault,
+                    identify: item.category_identify,
+                }
+                category.push(dataPush)
+                return
+            }
+            else{
+                category.forEach(element => {
+                    if(element.title === item.Category){
+                        checkExitsCategory = false
+                    }
+                })
+            }
+            if(checkExitsCategory === true){
+                let dataPush = {
+                    user_id : item.user_id,
+                    title: item.Category,
+                    is_visible: item.Category_visible,
+                    locale: localeDefault,
+                    identify: item.category_identify
+                }
+                category.push(dataPush)
+            }
+        })
+        categoryCreate = checkCategory(allCategory, category)
+        categoryCreate = generateCategoryIdentify(categoryCreate, allCategory)
+        // for(let i = 0; i < categoryCreate.length; i++){
+        //     let category_identify = localeDefault + user_id
+        //     categoryCreate[i].user_id = user_id
+        //     categoryCreate[i].identify = category_identify
+        // }
+        await createCategory(categoryCreate)
+        allCategory = await getAllCategory(user_id)
         // Prepare for Faqs data
-        const faqs = data.map(row => ({
-            user_id: user_id,
-            identify: row['identify'],
-            title: row['title'],
-            content: row['content'],
-            locale: row['locale'],
-            category_identify: row['category_identify'],
-            is_visible: row['is_visible'],
-            position: row['position'],
-        }));
-        // Prepare for category data
-        const uniqueCategory = Array.from(new Set(data.map(a => a.category_identify)))
-            .map(category_identify => {
-                return data.find(a => a.category_identify === category_identify)
-            })
-        const category = uniqueCategory.map(row => ({
-            user_id: user_id,
-            identify: row['category_identify'],
-            title: row['category name'],
-            is_visible: row['Category Visible'],
-            locale: row['locale']
-
-        }))
+        faqCreate = checkFaq(allFaq, faqs)
+        console.log(faqCreate)
+        for(let i = 0; i < faqCreate.length; i++){
+            for(let j = 0; j < allCategory.length; j++){
+                if(allCategory[j].title === faqCreate[i].category_name){
+                    // console.log(faqCreate[i].title)
+                    faqCreate[i].category_identify = allCategory[j].identify
+                    let identify = localeDefault + user_id + allCategory[j].identify
+                    identify = await generateFaqIdentify(faqCreate[i], identify)
+                    faqCreate[i].identify = identify
+                }
+            }
+        }
+        console.log(faqCreate)
+        // await createFaq(faqCreate)
+        // await importFaqs(categoryCreate, faqCreate)
         // Import faqs to database !
-        responseResult = await importFaqs(category, faqs);
+        // console.log(faqCreate)
+        // responseResult = await importFaqs(category, faqs);
     }
-    if (responseResult && !responseResult.status){
-        return res.status(responseResult.statusCode).send({
-            message:
-                responseResult.message || "Some error occurred while import Faqs."
-        });
-    }
+    // if (responseResult && !responseResult.status){
+    //     return res.status(responseResult.statusCode).send({
+    //         message:
+    //             responseResult.message || "Some error occurred while import Faqs."
+    //     });
+    // }
     return res.send({
         message: `Import data import successful !`
     });
+}
+
+async function generateFaqIdentify(faq, identify){
+    let count = 0;
+    let checked = false;
+    let newIdentify = identify;
+    do {
+        checked = false;
+        if (count) {
+            newIdentify = faq.identify + count;
+        }
+        await Faq.findOne({ where: { user_id: faq.user_id, identify: newIdentify, locale: localeDefault, category_identify: faq.category_identify}})
+        .then( async data => {
+            if (data) {
+                checked = true;
+            }
+        })
+        .catch(err => {
+            errorLog.error(`faq generate identify error ${err.message}`)
+        });
+        count++;
+    } while (checked);
+    return newIdentify;
 }
 
 exports.export = async (req, res) => {
@@ -92,29 +173,132 @@ exports.export = async (req, res) => {
     return res.send(buffer);
 }
 
-async function importFaqs(category, faqs) {
-    let responseResult = {};
-    responseResult.status = true;
-    // await Category.bulkCreate(category,
-    //     {
-    //         fields:['user_id', 'identify', 'title', 'locale', 'is_visible'],
-    //         updateOnDuplicate: ["identify", "user_id", "locale"]
-    //     }
-    //     ).then( async () => {
-    //         await Faq.bulkCreate(faqs,
-    //             {
-    //                 fields: ['user_id', 'identify', 'title', 'content', 'locale', 'category_identify', 'is_visible'],
-    //                 updateOnDuplicate: ["identify", "user_id", "category_identify", "locale"]
-    //             }).catch(errorLog => {
-    //                 responseResult.message = errorLog.message;
-    //                 responseResult.status = false;
-    //                 responseResult.statusCode = 500;
-    //         })
-    //     })
-    //     .catch(e=>{
-    //         responseResult.message = e.message;
-    //         responseResult.status = false;
-    //         responseResult.statusCode = 500;
-    // })
-    return responseResult;
+
+async function createCategory(category){
+    await Category.bulkCreate(category,{
+        fields:['user_id', 'identify', 'title', 'locale', 'is_visible'],
+        updateOnDuplicate: ["identify", "user_id", "locale"]
+    })
+    .then( async () => {
+        console.log('create success')
+    })
+    .catch(e=>{
+        console.log(e)
+    })
 }
+
+async function createFaq(faqs){
+    // console.log(faqs)
+    await Faq.bulkCreate(faqs,
+    {
+        fields: ['user_id', 'identify', 'title', 'content', 'locale', 'category_identify', 'is_visible'],
+        updateOnDuplicate: ["identify", "user_id", "category_identify", "locale"]            
+    }
+    ).then( async () => {
+        console.log('create success')
+    })
+    .catch(e=>{
+        console.log(e)
+    })
+}
+
+async function getAllCategory(user_id){
+    let allCategory = []
+    let condition = { user_id: user_id, locale: 'default' };
+    await Category.findAll({ where: condition })
+    .then(data => {
+        data.forEach(item => {
+            allCategory.push(item.dataValues)
+        })
+    })
+    .catch(err => {
+        console.log(err)
+    });    
+    return allCategory
+}
+
+function generateCategoryIdentify(categoryCreate, allCategory) {
+    // let newCategoryCreate = []
+    let count = 1;
+    // let newIdentify = identify;
+    categoryCreate.forEach(item => {
+        let checked = false;
+        allCategory.forEach(element => {
+            if(item.identify === element.identify){
+                checked = true
+            }
+        })
+        if(checked){
+            item.identify += count
+        }
+        else{
+            return
+        }
+        count++
+    })
+    return categoryCreate;
+}
+
+// async function checkCategoryIdentify(user_id, identify) {
+//     let checkedIdentify = false;
+//     await Category.findOne({ where: { user_id: user_id, identify: identify, locale: localeDefault}})
+//         .then( async data => {
+//             if (data) {
+//                 checkedIdentify = true;
+//             }
+//         }).catch(err => {
+//             errorLog.error(`category generate identify error ${err.message}`)
+//         });
+//     return checkedIdentify;
+// }
+
+async function getAllFaq(user_id){
+    let allFaq = []
+    let condition = { user_id: user_id, locale: 'default' };
+    await Faq.findAll({ where: condition })
+    .then(data => {
+        data.forEach(item => {
+            allFaq.push(item.dataValues)
+        })
+    })
+    .catch(err => {
+        console.log(err)
+    });    
+    return allFaq
+}
+
+function checkCategory(allCategory, category) {
+    let categoryCreate = []
+    var checkTitle
+    category.forEach(item => {
+        checkTitle = true
+        allCategory.forEach(element => {
+            if(item.title === element.title){
+                checkTitle = false
+            }
+        })
+        if(checkTitle === true){
+            categoryCreate.push(item)
+        }
+    })
+    return categoryCreate
+}
+
+function checkFaq(allFaq, faqs) {
+    let faqCreate = []
+    var checkTitle
+    faqs.forEach(item => {
+        checkTitle = true
+        allFaq.forEach(element => {
+            if(item.title === element.title){
+                checkTitle = false
+            }
+        })
+        if(checkTitle === true){
+            faqCreate.push(item)
+        }
+    })
+    
+    return faqCreate
+}
+
--- controller/defaultPage.js
+++ controller/defaultPage.js
@@ -88,7 +88,8 @@ exports.findFaqs = async (shop, locale, path_prefix = "") => {
 								replacements: [userID, listCategoryIdentify, locale],
 								type: QueryTypes.SELECT
 							}
-						);
+						)
+                        
 						dataCategories.forEach(item => {
 							if(item.locale === locale){
 								listCategory.push(item)
@@ -106,6 +107,7 @@ exports.findFaqs = async (shop, locale, path_prefix = "") => {
 							}
 						})
 					}
+
                     dataFaqs.forEach(item => {
                         if(item.locale === locale){
                             listFaq.push(item)
@@ -114,8 +116,9 @@ exports.findFaqs = async (shop, locale, path_prefix = "") => {
                             listFaqDefault.push(item)
                         }
                     })
-                    for(let i = 0; i < listFaqDefault.length; i++){
-                        for(let j = 0; j < listFaq.length; j++){
+
+                    for(let j = 0; j < listFaq.length; j++){
+                        for(let i = 0; i < listFaqDefault.length; i++){
                             if((listFaqDefault[i].identify === listFaq[j].identify && listFaqDefault[i].category_identify === listFaq[j].category_identify)){
                                 listFaqDefault.splice(i,1)
                             }
@@ -126,6 +129,7 @@ exports.findFaqs = async (shop, locale, path_prefix = "") => {
                             listFaq.push(item)
                         })
                     }
+
                     for(let i = 0; i < listFaq.length; i++){
                         for(let j = 0; j < listCategory.length; j++){
                             if(listFaq[i].category_categry === listCategory[j].category){
