87b33f85d051ae5a43b0eb3b0d00c30231686132
DungPham <phamanhdung593@gmail.com>
2022-12-15 13:40:18 +0700

    refs

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,17 +1,3 @@
-module.exports = {
-    HOST: "localhost",
-    USER: "dev_newuser",
-    PASSWORD: "dev_password",
-    DB: "dev_faq_app",
-    dialect: "mysql",
-    pool: {
-        max: 5,
-        min: 0,
-        acquire: 30000,
-        idle: 10000
-    }
-};
-
 // module.exports = {
 //     HOST: "localhost",
 //     USER: "root",
@@ -25,3 +11,17 @@ module.exports = {
 //         idle: 10000
 //     }
 // };
+
+module.exports = {
+    HOST: "localhost",
+    USER: "newuser",
+    PASSWORD: "password",
+    DB: "faq_app",
+    dialect: "mysql",
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+    }
+};
