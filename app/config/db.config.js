6e61741d3c2dd691e9df5c221004b9f87c34ce6e
DungPham <phamanhdung593@gmail.com>
2023-04-05 16:45:52 +0700

    refs

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,27 +1,27 @@
-module.exports = {
-  HOST: "localhost",
-  USER: "root",
-  PASSWORD: "Matkhau1",
-  DB: "purchase_order",
-  dialect: "mysql",
-  pool: {
-      max: 5,
-      min: 0,
-      acquire: 30000,
-      idle: 10000
-  }
-};
-
 // module.exports = {
-//     HOST: "localhost",
-//     USER: "dev_newuser",
-//     PASSWORD: "dev_password",
-//     DB: "dev_faq_app",
-//     dialect: "mysql",
-//     pool: {
-//         max: 5,
-//         min: 0,
-//         acquire: 30000,
-//         idle: 10000
-//     }
+//   HOST: "localhost",
+//   USER: "root",
+//   PASSWORD: "Matkhau1",
+//   DB: "purchase_order",
+//   dialect: "mysql",
+//   pool: {
+//       max: 5,
+//       min: 0,
+//       acquire: 30000,
+//       idle: 10000
+//   }
 // };
+
+module.exports = {
+    HOST: "localhost",
+    USER: "dev_newuser",
+    PASSWORD: "dev_password",
+    DB: "dev_faq_app",
+    dialect: "mysql",
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+    }
+};
