38e3df31999eba202b9248737c8aa0398a583963
DungPham <phamanhdung593@gmail.com>
2023-07-20 14:33:26 +0700

    refs

--- app/models/faq.model.js
+++ app/models/faq.model.js
@@ -46,10 +46,10 @@ module.exports = (sequelize, Sequelize) => {
         //   type: Sequelize.INTEGER,
         //   defaultValue: 0,
         // },
-        readed_faq: {
-          type: Sequelize.INTEGER,
-          defaultValue: 0,
-        },
+        // readed_faq: {
+        //   type: Sequelize.INTEGER,
+        //   defaultValue: 0,
+        // },
     },{
         indexes: [
             // add a FULLTEXT index
