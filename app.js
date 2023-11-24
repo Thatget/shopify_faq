9ed46e7d7ecdfed2c4be0f68297675c8e72868e5
Dung Pham <phamanhdung593@gmail.com>
2023-11-24 16:38:39 +0700

    ref

--- app.js
+++ app.js
@@ -513,7 +513,7 @@ app.set("loadMoreCategory","./loadMoreCategory");
 
 const defaultPage = require('./controller/defaultPage');
 
-let limit = 800
+let limit = 1000
 let setting_
 let category_
 let categoryRender = []
