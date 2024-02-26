3604d0091d6a743f6272bc22d831c957f10d973b
phamanhdung593 <phamanhdung593@gmail.com>
2024-02-26 16:53:57 +0700

    ref

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,3 +1,17 @@
+module.exports = {
+    HOST: "localhost",
+    USER: "dev_newuser",
+    PASSWORD: "dev_password",
+    DB: "dev_faq_app",
+    dialect: "mysql",
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+    }
+};
+
 // module.exports = {
 //     HOST: "localhost",
 //     USER: "root",
@@ -11,17 +25,3 @@
 //         idle: 10000
 //     }
 // };
-
-module.exports = {
-  HOST: "localhost",
-  USER: "newuser",
-  PASSWORD: "password",
-  DB: "faq_app",
-  dialect: "mysql",
-  pool: {
-      max: 5,
-      min: 0,
-      acquire: 30000,
-      idle: 10000
-  }
-};
